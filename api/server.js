777ea46698999912543d0acd8fb2f3ca6772a670
minntoko <minntoko0822@gmail.com>
2023-06-10 21:04:43 +0900

    add: JSON形式のリクエストを解析するためのミドルウェアを追加

--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
 const { PrismaClient } = require('@prisma/client');
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 
 const PORT = 8000;
 
 const prisma = new PrismaClient
 
+app.use(express.json());
+
 // 新規ユーザー登録API
-app.post("/api/auto/register", async (req, res) => {
+app.post("/api/auth/register", async (req, res) => {
     const { userName, email, password } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
