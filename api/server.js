d05cd2c5a86ab9d4aa4ffa2f801e528d5c3e1b49
minntoko <minntoko0822@gmail.com>
2023-06-10 21:28:28 +0900

    add: ユーザーログインを追加

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const { PrismaClient } = require('@prisma/client');
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const PORT = 8000;
 
@@ -23,6 +25,27 @@ app.post("/api/auth/register", async (req, res) => {
       }
     });
     return res.json({user});
+});
+
+// ユーザーログインAPI
+app.post("/api/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({where: {email}});
+  if (!user) {
+    return res.status(401).json({error: "ユーザーが見つかりません"});
+  }
+
+  const isPasswordVaild = await bcrypt.compare(password, user.password);
+  if (!isPasswordVaild) {
+    return res.status(401).json({error: "パスワードが間違っています"});
+  }
+
+  const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+
+  return res.json({token});
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
