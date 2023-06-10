3eacb4bdf0ddcea0d0ea86c96805d9f76e40363e
minntoko <minntoko0822@gmail.com>
2023-06-10 22:26:43 +0900

    fix: APIファイルを分割し、リファクタリングを行った

---
+++ api/routers/auth.js
@@ -0,0 +1,45 @@
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+
+const prisma = new PrismaClient();
+
+// 新規ユーザー登録API
+router.post("/register", async (req, res) => {
+  const { userName, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      userName,
+      email,
+      password: hashedPassword,
+    }
+  });
+  return res.json({user});
+});
+
+// ユーザーログインAPI
+router.post("/login", async (req, res) => {
+const { email, password } = req.body;
+
+const user = await prisma.user.findUnique({where: {email}});
+if (!user) {
+  return res.status(401).json({error: "ユーザーが見つかりません"});
+}
+
+const isPasswordVaild = await bcrypt.compare(password, user.password);
+if (!isPasswordVaild) {
+  return res.status(401).json({error: "パスワードが間違っています"});
+}
+
+const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {
+  expiresIn: "1d",
+});
+
+return res.json({token});
+});
+
+module.exports = router;
