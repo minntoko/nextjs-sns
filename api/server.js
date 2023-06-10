7a008824cf99cbbfc3e6f317ac9293b2b1007a71
minntoko <minntoko0822@gmail.com>
2023-06-10 20:36:28 +0900

    add: ユーザーを新規登録するapiを追加

--- api/server.js
+++ api/server.js
@@ -1,5 +1,23 @@
+const { PrismaClient } = require('@prisma/client');
 const express = require('express');
 const app = express();
 
 const PORT = 8000;
+
+const prisma = new PrismaClient
+
+// 新規ユーザー登録API
+app.post("/api/auto/register", async (req, res) => {
+    const { userName, email, password } = req.body;
+
+    const user = await prisma.user.create({
+      data: {
+        userName,
+        email,
+        password,
+      }
+    });
+    return res.json({user});
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
