dee97b1ddfa4c6cb0c5b6053c162e8263c632e57
minntoko <minntoko0822@gmail.com>
2023-06-11 20:38:00 +0900

    fix: corsオリジン制約に引っかからないように修正

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const authRoute = require('./routers/auth');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const PORT = 8000;
 
+app.use(cors());
 app.use(express.json());
-
 app.use("/api/auth", authRoute);
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
