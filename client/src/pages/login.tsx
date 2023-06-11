d1542717b51305bfab42ed1dcf02e0074d925e86
minntoko <minntoko0822@gmail.com>
2023-06-11 20:56:31 +0900

    add: APIと通信してログインする処理を追加

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,7 +1,33 @@
+import apiClient from "@/lib/apiClient";
 import Head from "next/head";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
-const login = () => {
+const Login = () => {
+  const [email, setEmail] = useState<String>("");
+  const [password, setPassword] = useState<String>("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // ここで新規登録を行うAPIを叩く
+    try {
+      const response = await apiClient.post("/auth/login", {
+        email: email,
+        password: password,
+      });
+
+      const token = response.data.token;
+      console.log(token);
+      
+
+      router.push("/");
+    } catch (error) {
+      alert("入力内容が正しくありません。");
+    }
+  };
   return (
     <div
       style={{ height: "88vh" }}
@@ -17,7 +43,7 @@ const login = () => {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="email"
@@ -32,6 +58,7 @@ const login = () => {
                 autoComplete="email"
                 required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
               />
             </div>
             <div className="mt-6">
@@ -48,6 +75,7 @@ const login = () => {
                 autoComplete="current-password"
                 required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
               />
             </div>
             <div className="mt-6">
@@ -65,4 +93,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
