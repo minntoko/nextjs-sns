b5f9e685d7fe5aa649ed5156bb2135373062acd1
minntoko <minntoko0822@gmail.com>
2023-06-11 19:59:41 +0900

    add: Timelineを追加

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Timeline from "@/components/Timeline";
 
 export default function Home() {
   return (
@@ -10,7 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div>
+        <Timeline />
+      </div>
     </>
   );
 }
