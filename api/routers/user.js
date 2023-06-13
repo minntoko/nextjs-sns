e0456ce2e7df84b81ab5fe07a74baa39332b8185
minntoko <minntoko0822@gmail.com>
2023-06-13 21:11:25 +0900

    add: ログインユーザーを取得するAPIを作成

---
+++ api/routers/user.js
@@ -0,0 +1,20 @@
+const { PrismaClient } = require("@prisma/client");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const router = require("express").Router();
+
+const prisma = new PrismaClient();
+
+router.get("/find", isAuthenticated, async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({where: {id: req.userId}});
+
+    if(!user) {
+      res.status(404).json({error: "ユーザーが見つかりませんでした。"});
+    }
+
+    res.status(200).json({user: {id: user.id, userName: user.userName, email: user.email}})
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
