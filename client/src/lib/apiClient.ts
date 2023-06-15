1efe431a34aaa43118289227b654fe6220c32002
minntoko <minntoko0822@gmail.com>
2023-06-15 20:11:33 +0900

    add: デプロイ用にenvファイル用の設定を追加

--- client/src/lib/apiClient.ts
+++ client/src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.BASE_URL || 'http://localhost:8000/api',
   headers: {
     "Content-Type": "application/json"
   }
